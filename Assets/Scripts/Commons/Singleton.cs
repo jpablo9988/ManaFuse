46c54cde14cf0b83f2599a333af78d583e47e247
Juan Pablo Amorocho <jpablo9988@gmail.com>
2025-03-09 16:09:17 -0400

    Added Singleton Common and Reconfigured Inputs to match planned ones
    New Inputs Added:
    Movement: Arrow Keys/Left Joystick
    Attacks: WASD (Named: AttackNorth, AttackWest, AttackEast, AttackSouth).
    Sprint: Left Shift, Left/Right Trigger.
    
    Added a new Common Class that contains all the functionality for Singletons in Unity.
    This Singleton Class should be used in rare circumstances, such as the GameContext class or something similar.

---
+++ Assets/Scripts/Commons/Singleton.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+/// Aggregates  all the basic Singleton Pattern needs into a single inheritable abstract class.
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindFirstObjectByType<T>();
+                if (_instance == null)
+                {
+                    GameObject singleton = new();
+                    _instance = singleton.AddComponent<T>();
+                    singleton.name = "(singleton) " + typeof(T).ToString();
+                    Debug.LogWarning("An instance of " + typeof(T) + " is missing in scene, creating a new one.");
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+        }
+        else if (_instance != this)
+        {
+            Destroy(this);
+            Debug.LogWarning($"An instance of {GetType().Name} already exists. Destroying this instance.");
+        }
+    }
+}
